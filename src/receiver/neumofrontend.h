d7b6e2f1ffe0733d27b6e321bf59ea7641f6e5a8
Deep Thought <deeptho@gmail.com>
2022-01-29 00:06:47 +0100

    Untested fix for issue #12. Note that this issue is NOT related to diseqc

--- src/receiver/neumofrontend.h
+++ src/receiver/neumofrontend.h
@@ -33,7 +33,6 @@ enum fe_extended_caps {
 	FE_CAN_SPECTRUMSCAN        = 0x01,
 	FE_CAN_IQ                  = 0x02,
 	FE_CAN_BLINDSEARCH         = 0x04,
-	FE_CAN_CONSTELLATION       = 0x10,
 	FE_CAN_MODCOD		   = 0x08
 };
 

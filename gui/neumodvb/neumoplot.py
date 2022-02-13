576f700432ba9fcb17733b8cafbd195d7285c040
Deep Thought <deeptho@gmail.com>
2022-02-13 20:01:45 +0100

    -

--- gui/neumodvb/neumoplot.py
+++ gui/neumodvb/neumoplot.py
@@ -887,7 +887,7 @@ class SpectrumPlot(wx.Panel):
                 self.set_current_annot(annot)
                 return
     def on_button_press(self, event):
-        print(f'CLICK SHIFT={self.shift_is_held} CTRL={self.ctrl_is_held}')
+        pass #print(f'CLICK SHIFT={self.shift_is_held} CTRL={self.ctrl_is_held}')
     def on_pick(self, event):
         """
         show/hide graph by clicking on legend line

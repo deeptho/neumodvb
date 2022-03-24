e064dbdf1ef6ae6e64e14ee29e55972ae204a646
Deep Thought <deeptho@gmail.com>
2022-03-24 23:26:27 +0100

    Fixes #12: BUG: tune transponder below 1000ks/s

--- src/receiver/frontend.cc
+++ src/receiver/frontend.cc
@@ -1077,7 +1077,7 @@ void cmdseq_t::init_pls_codes() {
 
 int dvb_frontend_t::tune(const chdb::lnb_t& lnb, const chdb::dvbs_mux_t& mux, const tune_options_t& tune_options) {
 	auto blindscan = tune_options.use_blind_tune || mux.delivery_system == chdb::fe_delsys_dvbs_t::SYS_AUTO
-		|| mux.symbol_rate < 1000000;
+		|| (mux.symbol_rate < 1000000 &&  ts.readAccess()->dbfe.supports.blindscan);
 	int num_constellation_samples = tune_options.constellation_options.num_samples;
 	tuned_frequency = mux.frequency;
 

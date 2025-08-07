614f47e7b3520dcf6a60c758c8a9c5de712d1425
Deep Thought <deeptho@gmail.com>
2025-08-07 17:42:17 +0200

    Do not save muxes when lock times out.

--- src/receiver/active_si_stream.cc
+++ src/receiver/active_si_stream.cc
@@ -305,6 +305,7 @@ void active_si_stream_t::finalize_scan_for_mux_(chdb::any_mux_t& mux_, bool is_m
 		c->scan_status = chdb::scan_status_t::IDLE;
 		c->scan_id = {};
 		c->scan_result = chdb::scan_result_t::NOLOCK; //mux could not be locked
+		nosave = is_template(mux);
 		break;
 	case tune_state_t::TUNE_FAILED:
 		preserve = m::flags{~m::MUX_COMMON};

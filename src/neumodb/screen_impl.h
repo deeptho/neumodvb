e0809b85949f1148515060f01b19eff31dcca223
Deep Thought <deeptho@gmail.com>
2026-02-01 00:45:45 +0100

    Silence compile warning

--- src/neumodb/screen_impl.h
+++ src/neumodb/screen_impl.h
@@ -680,14 +680,20 @@ record_t screen_t<record_t>::record_at_row(int row_number)
 	if(row_number != monitor.reference.row_number &&
 		 row_number != monitor.auxiliary_reference.row_number)
 #endif
+#ifndef NDEBUG
+		int count =
+#endif
 		set_reference(row_number);
 	if(row_number == monitor.reference.row_number)
 		return primary_current_record;
 	else if(row_number == monitor.auxiliary_reference.row_number)
 		return auxiliary_current_record;
+	dterrorf("count={} row_number={}", count, row_number);
+#if 1
 	assert(0);
 	//only for debugging
 	set_reference(row_number);
+#endif
 	return primary_current_record;
 }
 

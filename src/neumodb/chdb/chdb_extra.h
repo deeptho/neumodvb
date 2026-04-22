30fea7047ce71bc27983df4690352da517a0d123
Deep Thought <deeptho@gmail.com>
2026-04-22 22:23:59 +0200

    Update copyright notices

--- src/neumodb/chdb/chdb_extra.h
+++ src/neumodb/chdb/chdb_extra.h
@@ -1,6 +1,6 @@
 
 /*
- * Neumo dvb (C) 2019-2025 deeptho@gmail.com
+ * Neumo dvb (C) 2019-2026 deeptho@gmail.com
  * Copyright notice:
  *
  * This program is free software; you can redistribute it and/or modify
